4378a1c10cf2127e357ccccc12260d58c9fe4898
Granger <923481530@qq.com>
2022-09-06 11:18:04 +0800

    feat 初始化仓库

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,22 @@
 const { defineConfig } = require('@vue/cli-service')
+// const path = require('path')
+// const {join} = path
+const { join } = require('path')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // 关闭 eslint检查
+  //   关闭的是编译的时候
+  lintOnSave: false, // 配置原生选项
+  configureWebpack: {
+    devServer: {
+      open: true,
+      port: 8888,
+      host: 'localhost'
+    }, // 路径别名
+    resolve: {
+      alias: {
+        cpns: join(__dirname, 'src/components')
+      }
+    }
+  }
 })
